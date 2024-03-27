a1e74b27666ef3fe01a15ef2cd774713f80b472e
Sinaye <sinaye.mbele@gmail.com>
2024-03-27 08:42:01 +0200

    Adding JavaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+//Will look for document
+const navToggle = document.querySelector('.nav-toggle');
+
+//Getting each individual link
+const navLinks = document.querySelectorAll('.nav--link')
+
+//Nav toggle when clicked on
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+//Closes the nav link once we have clicked on what do we want to see
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
